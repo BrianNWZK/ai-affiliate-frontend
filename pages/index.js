a5c8b59ebddbf8e27c2d088fb22efadab94888ec
BrianNWZK <brianwaezike@gmail.com>
2025-06-16 23:57:58 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Welcome to AI Affiliate Marketing System</h1>
-    </div>
-  );
-}
