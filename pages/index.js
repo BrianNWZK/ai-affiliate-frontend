14d1c64ac811111df0642089ce6b0fc972439a08
BrianNWZK <brianwaezike@gmail.com>
2025-06-18 15:40:43 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import NeuralCommerceEcosystem from "../components/NeuralCommerceEcosystem";
+import AffiliateMarketingAI from "../components/AffiliateMarketingAI";
+
+export default function Home() {
+  return (
+    <div>
+      <NeuralCommerceEcosystem />
+      <AffiliateMarketingAI />
+    </div>
+  );
+}
