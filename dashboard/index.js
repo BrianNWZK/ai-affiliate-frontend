3526c7979681ee16b5a858bc4580bba387ba8fce
BrianNWZK <brianwaezike@gmail.com>
2025-06-18 15:23:30 -0700

    Delete dashboard directory

--- dashboard/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Welcome to AI Affiliate SaaS</h1>
-      <p>This is your homepage.</p>
-    </div>
-  );
-}
