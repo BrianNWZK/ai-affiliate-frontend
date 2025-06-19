4e0a3518e5e4a6ea5eb3fdf936f8e46d7a0378a9
BrianNWZK <brianwaezike@gmail.com>
2025-06-18 17:23:06 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import AffiliateMarketingAI from '@/components/AffiliateMarketingAI';
+import NeuralCommerceEcosystem from '@/components/NeuralCommerceEcosystem';
+
+export default function Home() {
+  return (
+    <main>
+      <NeuralCommerceEcosystem />
+      <AffiliateMarketingAI />
+    </main>
+  );
+}
